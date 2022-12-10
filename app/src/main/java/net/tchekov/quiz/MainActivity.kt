56dcc623b32ced2fd56da4eb6c104338dbbeefb6
Emil Tchekov <emil@tchekov.net>
2022-12-10 14:14:12 +0100

    Custom toast and other improvements

--- app/src/main/java/net/tchekov/quiz/MainActivity.kt
+++ app/src/main/java/net/tchekov/quiz/MainActivity.kt
@@ -1,14 +1,19 @@
 package net.tchekov.quiz
 
+import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import com.google.gson.GsonBuilder
 import java.io.InputStream
 import java.util.*
@@ -20,15 +25,17 @@ class MainActivity : AppCompatActivity() {
     private var questions: List<Question> = listOf()
     private var index = 0
 
-    private var quizName: String = "Quiz"
     private lateinit var textBoxQuestion: TextView
     private lateinit var textBoxInfo: TextView
     private lateinit var context: Context
+    private var reveal: Boolean = false
 
     /// <summary>
     /// Maximum achievable score
     /// </summary>
     private var highScores: Float = 0.0F
+    private var highScoresString = ""
+
 
     /// <summary>
     /// Achieved score
@@ -43,14 +50,14 @@ class MainActivity : AppCompatActivity() {
 
         val buttonReveal = findViewById<Button>(R.id.buttonReveal)
         buttonReveal.setOnClickListener {
-            for (box in answerBoxes) {
-                val answer = box.tag as Answer
-                if (answer.correct) {
-                    box.setTextColor(Color.RED)
-                } else {
-                    box.setTextColor(Color.BLACK)
-                }
+            reveal = if (buttonReveal.text == getString(R.string.reveal)) {
+                buttonReveal.setText(R.string.hide)
+                true
+            } else {
+                buttonReveal.setText(R.string.reveal)
+                false
             }
+            setCheckBoxColor()
         }
 
         val checkBoxA = findViewById<CheckBox>(R.id.checkboxA)
@@ -74,18 +81,41 @@ class MainActivity : AppCompatActivity() {
         val buttonNext = findViewById<Button>(R.id.buttonNext)
         buttonNext.setOnClickListener {
             var points = 0.0F
+            var achievable = 0.0F
             for (box in answerBoxes) {
-                if (box.isChecked) {
-                    val answer = box.tag as Answer
-                    if (answer.correct) points++
-                }
+                val answer = box.tag as Answer
+                if (answer.correct) achievable++
+                if (box.isChecked && answer.correct) points++
             }
+
             scored[index] = points
 
-            resetBoxes()
-            index++
-            askQuestion()
+            val dialog: Dialog
+            when (points) {
+                0.0F -> {
+                    dialog = createToast(getString(R.string.wrong), "#ffffff", "#ff0000")
+                }
+                achievable -> {
+                    dialog = createToast(getString(R.string.correct), "#000000", "#00ff00")
+                }
+                else -> {
+                    dialog = createToast(getString(R.string.incomplete), "#000000", "#ffc300")
+                }
+            }
+
+            object : CountDownTimer(1000, 1000) {
+                override fun onTick(p: Long) {}
+
+                override fun onFinish() {
+                    dialog.dismiss()
+                    resetBoxes()
+                    index++
+                    askQuestion()
+                }
+            }.start()
+
         }
+
 
         textBoxQuestion = findViewById(R.id.textview_question)
         textBoxInfo = findViewById(R.id.textview_info)
@@ -94,12 +124,13 @@ class MainActivity : AppCompatActivity() {
         val jsonString: String = Scanner(inputStream).useDelimiter("\\A").next()
         val gson = GsonBuilder().create()
         val quiz = gson.fromJson(jsonString, Quiz::class.java)
-        quizName = quiz.name
+        this.title = quiz.name
         questions = quiz.questions.shuffled()
         for (question in questions) {
             scored.add(0.0F)
             for (answer in question.answers) if (answer.correct) highScores++
         }
+        highScoresString = "%.0f".format(highScores)
         askQuestion()
     }
 
@@ -107,7 +138,23 @@ class MainActivity : AppCompatActivity() {
         for (box in checkBoxes) {
             box.visibility = View.INVISIBLE
             box.isChecked = false
-            box.setTextColor(Color.BLACK)
+        }
+
+        setCheckBoxColor()
+    }
+
+    private fun setCheckBoxColor() {
+        if (reveal) {
+            for (box in answerBoxes) {
+                val answer = box.tag as Answer
+                if (answer.correct) {
+                    box.setTextColor(Color.RED)
+                } else {
+                    box.setTextColor(Color.BLACK)
+                }
+            }
+        } else {
+            for (box in answerBoxes) box.setTextColor(Color.BLACK)
         }
     }
 
@@ -116,13 +163,14 @@ class MainActivity : AppCompatActivity() {
         for (score in scored) ownScore += score
         val percent = ownScore / highScores * 100
         val scoredPercent = "%.2f".format(percent)
+        val ownScoreString = "%.0f".format(ownScore)
 
         if (index >= questions.size) {
-            Toast.makeText(
-                context,
-                "Finished!\r\nScored: $ownScore/$highScores\r\n$scoredPercent",
-                Toast.LENGTH_LONG
-            ).show()
+            createToast(
+                "Finished!\r\nScored: $ownScoreString/$highScoresString\r\n$scoredPercent",
+                "#000000",
+                "#ffffff"
+            )
             return
         }
 
@@ -130,7 +178,7 @@ class MainActivity : AppCompatActivity() {
         val question = questions[index]
         textBoxQuestion.text = question.content
         val infoText =
-            "$quizName - Question: ${index + 1}/${questions.size} - ($ownScore/$highScores - $scoredPercent%)"
+            "Question ${index + 1}/${questions.size} - ($ownScoreString/$highScoresString - $scoredPercent%)"
         textBoxInfo.text = infoText
 
         val answerCount = question.answers.size - 1
@@ -148,5 +196,23 @@ class MainActivity : AppCompatActivity() {
             answerBoxes[i].text = answer.content
             answerBoxes[i].tag = answer
         }
+    }
+
+    private fun createToast(text: String, textColor: String, backgroundColor: String): Dialog {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.custom_toast)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val textView = dialog.findViewById<TextView>(R.id.toast_content)
+        val shape = textView.background.current
+        shape.clearColorFilter()
+        shape.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+            Color.parseColor(backgroundColor), BlendModeCompat.SRC_ATOP
+        )
+        textView.text = text
+        textView.setTextColor(Color.parseColor(textColor))
+        dialog.show()
+        return dialog
     }
 }
